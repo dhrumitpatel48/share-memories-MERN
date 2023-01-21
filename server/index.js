82644996288f9b8b8f687424faa6199e4ac6258d
Dhrumit Patel <dhrumitpatel48@gmail.com>
2023-01-21 13:08:13 +0530

    project started and initital setup has been done

--- server/index.js
+++ server/index.js
@@ -1,5 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import cors from cors;
+import cors from "cors";
+import dotnev from "dotenv";
 
+const app = express();
+dotnev.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+
+const PORT = process.env.PORT || 5000;
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(CONNECTION_URL, {
+    useUnifiedTopology: true,
+  })
+  .then(() => app.listen(PORT, console.log(`Server running on port ${PORT}`)))
+  .catch((error) => console.log(`Error while connecting mongodb : ${error}`));
