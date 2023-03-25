e2489523fb2c9a13bfe47643108944f8e1fbe85d
Dhrumit Patel <dhrumitpatel48@gmail.com>
2023-03-25 19:11:48 +0530

    Implemented Backend for JWT Auth

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import dotnev from "dotenv";
 import postRoutes from "./routes/posts.routes.js";
+import userRoutes from "./routes/users.routes.js";
 import logger from "./logger/logger.js";
 import "./utils/db.connection.js"; // Import mongodb connection
 
@@ -14,6 +15,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+app.use("/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
