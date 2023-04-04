f47d1ea6c0a58389c3b4ae8981036896528580fe
Dhrumit Patel <dhrumitpatel48@gmail.com>
2023-04-04 22:38:25 +0530

    implemented pagination frontend side

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { Container } from "@material-ui/core";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
+import PostDetails from "./components/PostDetails/PostDetails";
 
 const App = () => {
+  const user = JSON.parse(localStorage.getItem("profile"));
+  const navigate = useNavigate();
   return (
     <BrowserRouter>
       <Container maxWidth="lg">
         <Navbar />
         <Routes>
           <Route path="/" exact element={<Home />} />
-          <Route path="/auth" exact element={<Auth />} />
+          <Route path="/posts" exact element={<Home />} />
+          <Route path="/posts/search" exact element={<Home />} />
+          <Route path="/posts/:id" exact element={<PostDetails />} />
+          <Route path="/auth" exact element={!user ? <Auth /> : navigate("/")} />
         </Routes>
       </Container>
     </BrowserRouter>
