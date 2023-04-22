0dac4a027a5fe2470c8205fd568b61c69d8034cb
Dhrumit Patel <dhrumitpatel48@gmail.com>
2023-04-22 23:49:40 +0530

    Implemented loading

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -94,8 +94,8 @@ const Home = () => {
               </Button>
             </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
-            <Paper elevation={6}>
-              <Pagination page={page}/>
+            <Paper elevation={6} className={classes.pagination}>
+              <Pagination page={page} />
             </Paper>
           </Grid>
         </Grid>
