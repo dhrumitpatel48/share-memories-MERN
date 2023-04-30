d4ccd092cf3c5f17142b6a6affd9f6f11b3e458e
Dhrumit Patel <dhrumitpatel48@gmail.com>
2023-04-30 13:04:15 +0530

    Scroll comments list

--- client/src/components/PostDetails/CommentSection.jsx
+++ client/src/components/PostDetails/CommentSection.jsx
@@ -8,6 +8,7 @@ import { commentPost } from "../../actions/posts";
 const CommentSection = ({ post }) => {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const commentsRef = useRef();
   const [comments, setComments] = useState(post?.comments);
   const [comment, setComment] = useState("");
   const user = JSON.parse(localStorage.getItem("profile"));
@@ -16,6 +17,8 @@ const CommentSection = ({ post }) => {
     const newComments = await dispatch(commentPost(finalComment, post._id));
     setComments(newComments);
     setComment("");
+
+    commentsRef.current.scrollIntoView({ behavior: "smooth" });
   };
   return (
     <div>
@@ -26,9 +29,11 @@ const CommentSection = ({ post }) => {
           </Typography>
           {comments.map((c, i) => (
             <Typography key={i} gutterBottom variant="subtitle1">
-              {c}
+              <strong>{c.split(": ")[0]}</strong>
+              {c.split(":")[1]}
             </Typography>
           ))}
+          <div ref={commentsRef} />
         </div>
         {user?.result?.name && (
           <div style={{ width: "70%" }}>
